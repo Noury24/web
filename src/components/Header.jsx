a5cce723325cf0f7ab9479e5d0f9586e7cd04b2c
Noury24 <mohamudbashir91@gmail.com>
2024-09-21 00:30:35 +0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header () {
+    return ( 
+        <Header> 
+            <h1> 
+                <span>Noury</span> 
+                <span>WAB</span>
+            </h1> 
+            <form> 
+                <input type="text" placeholder='Search...'/>
+            </form>
+        </Header>
+        
+)
+}  
